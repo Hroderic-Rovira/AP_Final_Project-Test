37f42cb8162509e8b371b83370e25cb7a8ee8f9c
Hroderic-Rovira <66891623+Hroderic-Rovira@users.noreply.github.com>
2020-09-01 10:53:00 -0600

    Ready for Deployment
    Creo que ya estaría, no voy ha agregarle más features.
    
    Posibles updates podrían se agregados.

--- server.py
+++ server.py
@@ -19,17 +19,22 @@ HTML_TEMPLATE = 'main_page.html'
 @app.errorhandler(404)
 # Handler para el error 404.
 def error_404(e):
-    return jsonify(error=str(e)), 404
+    return render_template('error_404_page.html')
 
 @app.route('/', defaults = {'username': None, 'password': None})
+@app.route('/<username>', defaults = {'password': None})
 @app.route('/<username>/<password>')
 # URL para la página principal.
 def login_HTML(username, password):
-    if username and password:
-        if login(username, password, True) == "Invalid User":
-            return render_template('landing_page.html', login = "Invalid User")
+    if username:
+        if password:
+            if login(username, password, True) == "Invalid User":
+                print(username+"-"+password)
+                return render_template('landing_page.html', login = "Invalid User")
+            else:
+                return redirect('/main_page')
         else:
-            return redirect('/main_page')
+            return render_template('landing_page.html', login = "Invalid User")
     else: 
         return render_template('landing_page.html', login = "First Time")
 
@@ -43,12 +48,12 @@ def main_page_HTML():
 @app.route('/products/all')
 def listar_productos_HTML():
     # URL para listar los productos.
-    return render_template('productos.html', rendered_request='/api/products/all', rendered_response=listar_productos(True), name = "listar" )
+    return render_template('productos.html', rendered_response=listar_productos(True), name = "listar" )
 
 @app.route('/products/detail/<idProducto>')
 def filtrar_productos_HTML(idProducto):
     # URL para filtrar los productos.
-    return render_template('productos.html', rendered_request=f'/api/products/detail/{idProducto}', rendered_response=filtrar_productos(idProducto,True), name = "filtrar" )
+    return render_template('productos.html', rendered_response=filtrar_productos(idProducto,True), name = "filtrar" )
 
 # -|- URL de Productos con prefijo -> /api -|-
 
@@ -61,7 +66,7 @@ def listar_productos(flag = None):
         return jsonify({"productos": controller_Productos.listar_productos()})
 
 @app.route('/api/products/detail/<idProducto>', methods=['GET'])
-def filtrar_productos(idProducto, flag = None): #! Check why it doesn't work.
+def filtrar_productos(idProducto, flag = None):
     # Genera el request para filtrar un producto.
     try:
         if flag:
@@ -69,32 +74,28 @@ def filtrar_productos(idProducto, flag = None): #! Check why it doesn't work.
         else:
             return jsonify({idProducto: controller_Productos.buscar_productos(int(idProducto))})
     except ValueError:
-        return make_response(jsonify({"message": f'El parámetro idProducto ({idProducto}) debe ser un número.'}), 400)
+        return make_response(jsonify({"Mensaje de Error": f'El parámetro idProducto ({idProducto}) debe ser un número.'}), 400)
 
 # endregion
 
 # region Carrito:
 
-
 @app.route('/cart/<idUsuario>')
 def ver_carrito_HTML(idUsuario):
     # URL para ver los contenidos de un usuario.
-    return render_template('carrito.html', rendered_request=f'/api/products/detail/{idUsuario}', rendered_response=ver_carrito(idUsuario,True), username = filtrar_usuarios(idUsuario,True))
-
+    return render_template('carrito.html', rendered_response=ver_carrito(idUsuario,True), username = filtrar_usuarios(idUsuario,True))
 
 @app.route('/cart/price/<idUsuario>')
 def ver_precio_total_HTML(idUsuario):
     # URL para ver el precio total de un carrito.
-    return render_template('total_pagar.html', render_request=f'/api/cart/{idUsuario}', rendered_response=ver_precio_total(idUsuario,True), username = filtrar_usuarios(idUsuario,True))
-
+    return render_template('total_pagar.html', rendered_response=ver_precio_total(idUsuario,True), username = filtrar_usuarios(idUsuario,True))
 
 @app.route('/cart/qty/<idUsuario>')
 def ver_cantidad_total_HTML(idUsuario):
     # URL para ver la cantidad total de productos en un carrito.
-    return render_template('total_cantidad.html', render_request=f'/api/cart/{idUsuario}', rendered_response=ver_cantidad_total(idUsuario, True), username = filtrar_usuarios(idUsuario,True))
+    return render_template('total_cantidad.html', rendered_response=ver_cantidad_total(idUsuario, True), username = filtrar_usuarios(idUsuario,True))
 
 # -|- URL de Carrito con prefijo -> /api -|-
-
 
 @app.route('/api/cart/<idUsuario>', methods=['GET'])
 def ver_carrito(idUsuario, flag = None):
@@ -106,7 +107,7 @@ def ver_carrito(idUsuario, flag = None):
             return jsonify({idUsuario: controller_Carrito.filtrar_carritos(int(idUsuario))})
 
     except ValueError:
-        return make_response(jsonify({"message": f'El parametro idUsuario debe ser un número'}), 400)
+        return make_response(jsonify({"Mensaje de Error": f'El parametro idUsuario debe ser un número.'}), 400)
 
 
 @app.route('/api/cart/<idUsuario>/<idProducto>', methods=['POST'])
@@ -116,24 +117,22 @@ def agregar_producto_carrito(idUsuario, idProducto):
         mensaje = controller_Carrito.agregar_item_Carrito(
             int(idUsuario), int(idProducto), 1)
         if mensaje:
-            return jsonify({"message": f'El producto {mensaje} fue añadido.'})
+            return jsonify({"Mensaje": f'El producto {mensaje} fue añadido.'})
         else:
-            return jsonify({"message": f'El producto {mensaje} no pudo ser añadido.'})
+            return jsonify({"Mensaje de Error": f'El producto {mensaje} no pudo ser añadido.'})
 
     except ValueError:
-        return make_response(jsonify({"message": f'El parametro idUsuario debe ser un número'}), 400)
+        return make_response(jsonify({"Mensaje de Error": f'El parametro idUsuario debe ser un número'}), 400)
     except Exception as mensaje_error:
-        return make_response(jsonify({"message": str(mensaje_error)}), 400)
-
+        return make_response(jsonify({"Mensaje de Error": str(mensaje_error)}), 400)
 
 @app.route('/api/cart/<idUsuario>/<idProducto>', methods=['DELETE'])
 def eliminar_producto_carrito(idUsuario, idProducto):
     # Genera el request para eliminar un producto de un carrito.
     try:
-        return jsonify({"message": controller_Carrito.borrar_item_Carrito(int(idUsuario), int(idProducto), 1)})
+        return jsonify({"Mensaje": controller_Carrito.borrar_item_Carrito(int(idUsuario), int(idProducto), 1)})
     except ValueError:
-        return make_response(jsonify({"message": f'Los parametros idUsuario y idProducto debe ser números'}), 400)
-
+        return make_response(jsonify({"Mensaje de Error": f'Los parametros idUsuario y idProducto debe ser números.'}), 400)
 
 @app.route('/api/cart/price/<idUsuario>', methods=['GET'])
 def ver_precio_total(idUsuario, flag = None):
@@ -146,7 +145,6 @@ def ver_precio_total(idUsuario, flag = None):
     except ValueError:
         return make_response(jsonify({"message": f'El parametro idUsuario debe ser un número'}), 400)
 
-
 @app.route('/api/cart/qty/<idUsuario>', methods=['GET'])
 def ver_cantidad_total(idUsuario, flag = None):
     # Genera el request para visualizar la cantidad total de productos en un carrito.
@@ -157,7 +155,6 @@ def ver_cantidad_total(idUsuario, flag = None):
             return jsonify({idUsuario: controller_Carrito.calcular_Cantidad_Total(int(idUsuario))})
     except ValueError:
         return make_response(jsonify({"message": f'El parametro idUsuario debe ser un número'}), 400)
-
 
 @app.route('/api/cart/<idUsuario>', methods=['DELETE'])
 def comprar_productos(idUsuario):
@@ -232,4 +229,4 @@ def login(username, password, flag = None):
 
 # endregion
 
-app.run(host=HOST_ADRESS, debug = True)
+app.run(host=HOST_ADRESS, port= 80)
