3a5677df0c618d5e5217708c7f89d870411c715c
Hroderic-Rovira <66891623+Hroderic-Rovira@users.noreply.github.com>
2020-09-04 17:33:00 -0600

    Implementing OAuth 2.0 y Swagger

--- server.py
+++ server.py
@@ -1,18 +1,54 @@
 from flask import Flask
-from flask import request, render_template, jsonify, make_response, abort, redirect
-
+from flask import request, render_template, jsonify, make_response, abort, redirect, url_for
 from controllers import Controller_Producto, Controller_Carritos, Controller_Usuarios
 
+#  - GitHub OAuth 2.0
+from flask_github import GitHub
+
+# region Variables Goblales
+
 app = Flask(__name__, template_folder='views')
-usuario_logeado = None
+
 controller_Carrito = Controller_Carritos()
 controller_Productos = Controller_Producto()
-controller_Usuarios = Controller_Usuarios()
+controller_Usuarios = Controller_Usuarios() 
 
-# region Variables Globales
+#region GITHUB OAUTH 
+    # (LOCAL):
+# app.config['GITHUB_CLIENT_ID'] = '31437e8463c8e7e1768e'
+# app.config['GITHUB_CLIENT_SECRET'] = '51e3e9ba596ae39946963c0639588366bb3b1da0'
+
+# github = GitHub(app)
+
+    # (PÚBLICO):
+app.config['GITHUB_CLIENT_ID'] = '31437e8463c8e7e1768e'
+app.config['GITHUB_CLIENT_SECRET'] = '51e3e9ba596ae39946963c0639588366bb3b1da0'
+
+github = GitHub(app)
+#endregion
+
+# region Constantes
 
 HOST_ADRESS = '127.0.0.1'
 HTML_TEMPLATE = 'main_page.html'
+
+# endregion
+
+@app.route("/git_aut/")
+# Se invoca el OAuth de GitHub.
+def login_github():
+    return github.authorize()
+
+@app.route('/github-callback')
+@github.authorized_handler
+# Nuestro call back the GitHub, donde se determina si el usuario autorizó nuestra aplicación.
+def authorized_git(oauth_token):
+    next_url = request.args.get('next') or url_for('login_HTML')
+    if oauth_token is None:
+        return redirect(next_url)
+    else:
+        next_url = url_for('main_page_HTML')
+    return redirect(next_url)
 
 # endregion
 
@@ -30,7 +66,7 @@ def login_HTML(username, password):
         if password:
             if login(username, password, True) == "Invalid User":
                 print(username+"-"+password)
-                return render_template('landing_page.html', login = "Invalid User")
+                return render_template('landing_page.html', login = "Invalid User", error = "Los datos no coinciden con ningún usuario.")
             else:
                 return redirect('/main_page')
         else:
@@ -229,4 +265,4 @@ def login(username, password, flag = None):
 
 # endregion
 
-app.run(host=HOST_ADRESS, port= 80)
+app.run(host=HOST_ADRESS, port= 80, debug=True)
